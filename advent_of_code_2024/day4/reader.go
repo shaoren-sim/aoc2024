d46c460e00b0590295d81a7db4a9140ffebadf4c
shaoren <shaoren.sim@gmail.com>
2024-12-05 09:54:57 +0800

    Add download functionality.

--- advent_of_code_2024/day4/reader.go
+++ advent_of_code_2024/day4/reader.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"io"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -14,17 +18,64 @@ func StringToLines(s string) (lines []string, err error) {
 	return lines, nil
 }
 
-func GetInputLines() []string {
-	// Download input by emulating curl command.
-	var inputURL string = "https://adventofcode.com/2024/day/4/input"
-	var sessionCookie string = GetCookieFromEnvVar()
+func getDownloadedFile(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	// If the file already exists.
+	if !errors.Is(err, os.ErrNotExist) {
+		b, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Cast []byte to []string
+		bString := string(b)
 
-	input := DownloadInputWithCookie(inputURL, sessionCookie)
+		return bString, nil
+	} else {
+		return "", err
+	}
+}
+
+func writeStringToFile(input string, filePath string) error {
+	// Open the file with deferred close.
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(input)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetInputLines() []string {
+	// Save download to file.
+	const downloadedFile string = "input.txt"
+
+	// Try to get a pre-downloaded file.
+	input, err := getDownloadedFile(downloadedFile)
+	if err != nil {
+		// If parsing the downladed file failed,
+		// Download input by emulating curl command.
+		var inputURL string = "https://adventofcode.com/2024/day/4/input"
+		var sessionCookie string = GetCookieFromEnvVar()
+
+		input = DownloadInputWithCookie(inputURL, sessionCookie)
+
+		// After downloading, save a copy of the file.
+		writeErr := writeStringToFile(input, downloadedFile)
+		if writeErr != nil {
+			panic(writeErr)
+		}
+	}
 
 	// Here, lines are in the format of [][]string
 	parsedLines, err := StringToLines(input)
 	if err != nil {
-		panic("Problem downloading file")
+		panic("Problem parsing file")
 	}
 
 	return parsedLines
