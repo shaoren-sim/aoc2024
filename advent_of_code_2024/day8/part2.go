ed5f437b113b942744fcc6366b427292824b0acf
Shao Ren <shaoren.sim@gmail.com>
2024-12-08 15:01:04 +0800

    day8 part2 solved.

--- advent_of_code_2024/day8/part2.go
+++ advent_of_code_2024/day8/part2.go
@@ -1,9 +1,139 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func GetPossibleAntinodesPart2(positions [][2]int, dimX int, dimY int) [][2]int {
+	// Given the positions, find all the possible antinode locations.
+	// This includes nodes that will need to be filtered later on, i.e. out of range.
+	antinodes := make([][2]int, 0)
+
+	for i, positionA := range positions {
+		// Check all combinations
+		for j, positionB := range positions {
+			// For the same element, add the node to the list.
+			// Since the loop can enter the j-loop, this means there is more than 1 antenna.
+			if i == j {
+				antinodes = append(
+					antinodes,
+					positionA,
+				)
+				continue
+			}
+
+			vectorA := GetVector(positionA, positionB)
+			// Add new antinodes until out of range.
+			aMult := 1
+			for {
+				// Compute position
+				newX := positionB[0] + vectorA[0]*aMult
+				newY := positionB[1] + vectorA[1]*aMult
+
+				// Break if out of range.
+				if newX < 0 || newX >= dimX || newY < 0 || newY >= dimY {
+					break
+				}
+				antinodes = append(
+					antinodes,
+					[2]int{newX, newY},
+				)
+				aMult += 1
+			}
+			// fmt.Println(positionA, positionB, vectorA, antinodeA)
+
+			// Get the negative vector
+			vectorB := [2]int{-vectorA[0], -vectorA[1]}
+			aMult = 1
+			for {
+				// Compute position
+				newX := positionA[0] + vectorB[0]*aMult
+				newY := positionA[1] + vectorB[1]*aMult
+
+				// Break if out of range.
+				if newX < 0 || newX >= dimX || newY < 0 || newY >= dimY {
+					break
+				}
+				antinodes = append(
+					antinodes,
+					[2]int{newX, newY},
+				)
+				aMult += 1
+			}
+
+			antinodeB := [2]int{positionA[0] + vectorB[0], positionA[1] + vectorB[1]}
+			antinodes = append(antinodes, antinodeB)
+			// fmt.Println(positionA, positionB, vectorB, antinodeB)
+		}
+	}
+
+	return antinodes
+}
+
+func testSolve2(filepath string, expected int) {
+	const blankChar string = "."
+
+	input, err := getDownloadedFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Here, lines are in the format of [][]string
+	lines, err := GetLines(input)
+	if err != nil {
+		panic("Problem parsing file")
+	}
+
+	// Track height and width
+	dimX := len(lines[0])
+	dimY := len(lines)
+
+	// Get the positions of each unique character.
+	positionsMap := ParsePositions(lines, blankChar)
+	// for k, v := range positionsMap {
+	// 	fmt.Println(k, v)
+	// }
+
+	possibleAntinodes := make([][2]int, 0)
+	for _, positions := range positionsMap {
+		possibleAntinodes = append(
+			possibleAntinodes,
+			GetPossibleAntinodesPart2(positions, dimX, dimY)...)
+	}
+
+	// Filter out antinodes that break the rules.
+	antinodes := FilterAntinodes(possibleAntinodes, dimX, dimY)
+
+	if len(antinodes) != expected {
+		fmt.Println("Got", len(antinodes), ", expected", expected)
+		panic("Failed test")
+	}
+}
 
 func MainPart2() {
-	fmt.Printf("Answer Part 2: %d\n", 0)
+	testSolve2("test.txt", 34)
+
+	const blankChar string = "."
+
+	lines := GetInputs()
+
+	// Track height and width
+	dimX := len(lines[0])
+	dimY := len(lines)
+	// fmt.Println("Input width (X):", dimX)
+	// fmt.Println("Input height (Y):", dimY)
+
+	// Get the positions of each unique character.
+	positionsMap := ParsePositions(lines, blankChar)
+
+	possibleAntinodes := make([][2]int, 0)
+	for _, positions := range positionsMap {
+		possibleAntinodes = append(
+			possibleAntinodes,
+			GetPossibleAntinodesPart2(positions, dimX, dimY)...)
+	}
+	// fmt.Println("Number of antinodes before filtering:", len(possibleAntinodes))
+
+	// Filter out antinodes that break the rules.
+	antinodes := FilterAntinodes(possibleAntinodes, dimX, dimY)
+
+	fmt.Printf("Answer Part 2: %d\n", len(antinodes))
 }
